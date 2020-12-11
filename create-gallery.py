ed781e2f6978fd5c323140a20bd20f28b89d7fd6
Shaked Zychlinski <shakedzy@gmail.com>
2020-12-11 12:46:58 +0200

    tel aviv and hachula galleries

--- create-gallery.py
+++ create-gallery.py
@@ -7,7 +7,9 @@ if __name__ == "__main__":
 	parser.add_argument('-p', '--preview', dest='preview', required=True, help='Image number of preview photo')
 	parser.add_argument('-t', '--title', dest='title', help='Gallery title')
 	parser.add_argument('-z', '--zeroes', dest='zeroes', default=4, help='Number of total figures in files names')
-	parser.add_argument('-s', '--skip-overview', dest='skip_overview', default=False, action='store_true', help="Don't add gallery to overview.yml")
+	parser.add_argument('-s', '--skip', dest='skip_all', default=False, action='store_true', help="Skip both overview and gallery")
+	parser.add_argument('-so', '--skip-overview', dest='skip_overview', default=False, action='store_true', help="Don't add gallery to overview.yml")
+	parser.add_argument('-sg', '--skip-gallery', dest='skip_gallery', default=False, action='store_true', help="Don't add gallery md file")
 	args = parser.parse_args()
 
 	SUFFIX = '.JPG'
@@ -25,17 +27,17 @@ if __name__ == "__main__":
 
 	with open(os.getcwd() + f'/_data/galleries/{args.gallery_name}.yml', 'w+') as f:
 		f.write(f'picture_path: {args.gallery_name} \n')
-		f.write('preview: \n')
-		f.write(f'  filename: {get_image_name(args.preview)} \n')
-		f.write(f'  original: {get_image_name(args.preview, original=True)} \n')
-		f.write(f'  thumbnail: {get_image_name(args.preview, thumbnail=True)} \n')
+		# f.write('preview: \n')
+		# f.write(f'  filename: {get_image_name(args.preview)} \n')
+		# f.write(f'  original: {get_image_name(args.preview, original=True)} \n')
+		# f.write(f'  thumbnail: {get_image_name(args.preview, thumbnail=True)} \n')
 		f.write('pictures: \n')
 		for i in range(1, num_of_photos+1):
 			f.write(f'- filename: {get_image_name(i)} \n')
 			f.write(f'  original: {get_image_name(i, original=True)} \n')
 			f.write(f'  thumbnail: {get_image_name(i, thumbnail=True)} \n')
 
-	if not args.skip_overview:
+	if not (args.skip_all or args.skip_overview):
 		with open(os.getcwd() + '/_data/galleries/overview.yml', 'r+') as f:
 			content = f.read()
 			f.seek(0, 0)
@@ -47,3 +49,13 @@ if __name__ == "__main__":
 			f.write(f'  original: {get_image_name(args.preview, original=True)} \n')
 			f.write(f'  thumbnail: {get_image_name(args.preview, thumbnail=True)} \n')
 			f.write(content)
+
+	if not (args.skip_all or args.skip_gallery):
+		with open(os.getcwd() + f'/galleries/{args.gallery_name}.md', 'w+') as f:
+			f.write('--- \n')
+			f.write('layout: gallery \n')
+			f.write(f'title: {args.title} \n')
+			f.write('no_menu_item: true \n') 
+			f.write('support: [jquery, gallery] \n')
+			f.write('--- \n\n\n')
+			f.write('{% include gallery-layout.html gallery=site.data.galleries.' + args.gallery_name + ' %} \n')
---
+++ galleries/tel_aviv_prime.md
@@ -0,0 +1,15 @@
+---
+layout: gallery
+title: Tel Aviv Through a Prime Lens
+no_menu_item: true 
+support: [jquery, gallery]
+---
+
+One of the things I realized during COVID time, is that I really like to photograph - and I should be doing it more. And so I decided to give myself a little push in that direction with a little gift, because anyway everything is closed now and I can't spend my money on anything. I've decided to  myself with the one thing I didn't understand why people use - a prime lens. Why do some of us choose to shoot without zoom? I had no idea, but I'm about to find out.
+
+I examine what focal length I usually use, and realized I mostly shoot using wide angles with my kit lens. And so, after reading some reviews, I decided to get myself the Canon EF-S 24mm f/2.8 STM pancake lens. The moment I took it out of the box I was struck with awe, as I realized how light it is compared to my 18-135 kit lens. I was afraid I'll drop the camera due to the fact I wasn't used to it being so light. It was awesome.
+
+The day after it arrived I took a day off of work, took the camera and grabbed a taxi to the southern - and much more colorful - part of the city, and started practicing. It took me me sometime to understand how to shoot without zomming in or out, and how to wisely use other parameters - mainly the aperture size - but once this day was over, I felt like I made terrific progress. I could really see how I devloped throughout the days when comparing my first photos of the day, from the Carmel Market, to the ones I took later in Neve Tzedek and near the sea. Here are some of my favorites of the day:
+
+{% include gallery-layout.html gallery=site.data.galleries.tel_aviv_prime %}
+
