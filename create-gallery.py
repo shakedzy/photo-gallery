acec6cd765519ba1abfc078ef3ec03a6db95f77c
Shaked Zychlinski <shakedzy@gmail.com>
2020-12-13 23:39:28 +0200

    Add EXIF

--- create-gallery.py
+++ create-gallery.py
@@ -1,17 +1,19 @@
 import os
 import argparse
-from PIL import Image
+from PIL import Image, ExifTags
+from datetime import datetime
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-n', '--name', dest='gallery_name', required=True, help='Gallery directory name')
 	parser.add_argument('-p', '--preview', dest='preview', required=True, help='Image number of preview photo')
 	parser.add_argument('-t', '--title', dest='title', required=True, help='Gallery title')
-	parser.add_argument('-d', '--date', dest='date', required=True, help='Gallery shooting date')
+	parser.add_argument('-d', '--date', dest='date', help='Gallery shooting date')
 	parser.add_argument('-z', '--zfill', dest='zfill', default=4, help='Number of total figures in files names')
 	parser.add_argument('-x', '--suffix', dest='suffix', default="JPG", help="Files suffix")
 	parser.add_argument('-ts', '--thumbnail-size', dest='thumbnail_size', default='600,400', help="Width and height of thumbnails")
 	parser.add_argument('-st', '--skip-thumbs', dest='skip_thumbnails', default=False, action='store_true', help="Don't create thumbnails (assume they already exist)")
+	parser.add_argument('-se', '--skip-exif', dest='skip_exif', default=False, action='store_true', help="Don't enrich EXIF metadata")
 	parser.add_argument('-so', '--skip-overview', dest='skip_overview', default=False, action='store_true', help="Don't add gallery to overview.yml")
 	parser.add_argument('-sg', '--skip-gallery', dest='skip_gallery', default=False, action='store_true', help="Don't add gallery md file")
 	args = parser.parse_args()
@@ -21,17 +23,17 @@ if __name__ == "__main__":
 	num_of_photos = len(photos)
 	prefix = photos[0].split('-', 1)[0]
 
-	def get_image_name(n, original=False, thumbnail=False):
-		s = f'{prefix}-{str(n).zfill(args.zfill)}'
-		if thumbnail: s += f'-thumbnail.{args.suffix}'
-		elif original: s += f'.{args.suffix}'
-		return s
+	def get_image_name_from_id(n):
+		return f'{prefix}-{str(n).zfill(args.zfill)}'
+
+	def get_image_thumbnail_name(name):
+		return f'{name}-thumbnail.{args.suffix}'
 
 	if not args.skip_thumbnails:
 		width, height = [int(i) for i in args.thumbnail_size.split(',')]
 		for photo in photos:
 			thumbnail_size = (width, width) if width > height else (height, height)
-			thumbnail_filename = f'{photo[:-(len(args.suffix)+1)]}-thumbnail.{args.suffix}'
+			thumbnail_filename = get_image_thumbnail_name(photo)
 			im = Image.open(os.path.join(photos_path, photo))
 			im.thumbnail(thumbnail_size, Image.ANTIALIAS)
 			im.save(os.path.join(photos_path, thumbnail_filename))
@@ -40,12 +42,29 @@ if __name__ == "__main__":
 		f.write(f'picture_path: {args.gallery_name} \n')
 		f.write('pictures: \n')
 		for i in range(1, num_of_photos+1):
-			f.write(f'- filename: {get_image_name(i)} \n')
-			f.write(f'  original: {get_image_name(i, original=True)} \n')
-			f.write(f'  thumbnail: {get_image_name(i, thumbnail=True)} \n')
+			photo = get_image_name_from_id(i)
+			f.write(f'- filename: {photo} \n')
+			f.write(f'  original: {photo}.{args.suffix} \n')
+			f.write(f'  thumbnail: {get_image_thumbnail_name(photo)} \n')
+			if not args.skip_exif:
+				f.write('  exif: \n')
+				im = Image.open(os.path.join(photos_path, f'{photo}.{args.suffix}'))
+				exif = dict()
+				exif_info = {ExifTags.TAGS[k]: v for k, v in im._getexif().items() if k in ExifTags.TAGS}
+				exif['model'] = exif_info.get('Model', '')
+				exif['lens'] = exif_info.get('LensModel', None)
+				exif['aperture'] = str(exif_info['FNumber'][0] / exif_info['FNumber'][1]) if 'FNumber' in exif_info else ''
+				exif['shutter'] = f"{exif_info['ExposureTime'][0]}/{exif_info['ExposureTime'][1]}" if 'ExposureTime' in exif_info else ''
+				exif['focal'] = int(exif_info['FocalLength'][0] / exif_info['FocalLength'][1]) if 'FocalLength' in exif_info else ''
+				exif['iso'] = exif_info.get('ISOSpeedRatings', '')
+				exif['datetime'] = exif_info.get('DateTime', '')
+				for k,v in exif.items():
+					if v is not None:
+						f.write(f'    {k}: {v} \n')
 
 	if not args.skip_overview:
 		with open(os.getcwd() + '/_data/galleries/overview.yml', 'r+') as f:
+			photo = get_image_name_from_id(args.preview)
 			content = f.read()
 			f.seek(0, 0)
 			f.write('- \n')
@@ -53,9 +72,9 @@ if __name__ == "__main__":
 			f.write(f' directory: {args.gallery_name} \n')
 			f.write(f' date: {args.date} \n')
 			f.write(' preview: \n')
-			f.write(f'  filename: {get_image_name(args.preview)} \n')
-			f.write(f'  original: {get_image_name(args.preview, original=True)} \n')
-			f.write(f'  thumbnail: {get_image_name(args.preview, thumbnail=True)} \n')
+			f.write(f'  filename: {photo} \n')
+			f.write(f'  original: {photo}.{args.suffix} \n')
+			f.write(f'  thumbnail: {get_image_thumbnail_name(photo)} \n')
 			f.write(content)
 
 	if not args.skip_gallery:
